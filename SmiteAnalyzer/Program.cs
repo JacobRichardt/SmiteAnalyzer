b639e5275e4e543807f781bb773310e0d1dbc7b2
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-01-26 02:18:57 +0100

    Added basic console app.

---
+++ SmiteAnalyzer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace SmiteAnalyzer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Start Smite Analyzer");
+
+			var path = ConfigurationManager.AppSettings.Get("SmitePath");
+
+			Console.WriteLine("Checking for smite at \"{0}\"", path);
+
+			if (Directory.Exists(path))
+			{
+				Console.WriteLine("Locations exists");
+			}
+			else
+				Console.WriteLine("Smite not found");
+
+
+			if (ConfigurationManager.AppSettings.Get("WaitOnDone") == "true")
+			{
+				Console.WriteLine("Done. Press any key to exit");
+				Console.ReadKey();
+			}
+			else
+			{
+				Console.WriteLine("Done");
+			}
+		}
+	}
+}
