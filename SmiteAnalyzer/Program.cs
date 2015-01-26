1699b3a9892f5e9d295d1e7edcc8eb22af280fa9
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-01-26 02:52:38 +0100

    Added some package analyzing.

--- SmiteAnalyzer/Program.cs
+++ SmiteAnalyzer/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Configuration;
 using System.IO;
+using UELib;
+using UELib.Core;
 
 namespace SmiteAnalyzer
 {
 	class Program
 	{
 		static void Main(string[] args)
+		{
+			new Program().Run();
+		}
+
+		private void Run()
 		{
 			Console.WriteLine("Start Smite Analyzer");
 
@@ -17,10 +24,18 @@ namespace SmiteAnalyzer
 			if (Directory.Exists(path))
 			{
 				Console.WriteLine("Locations exists");
+
+				Console.WriteLine("Checking for Potraits file");
+
+				var potraitsPath = Path.Combine(ConfigurationManager.AppSettings.Get("SmitePath"), "BattleGame\\CookedPC\\GUI\\Icons\\Portraits.upk");
+
+				if (File.Exists(potraitsPath))
+					AnalyzePotraits(potraitsPath);
+				else
+					Console.WriteLine("Potraits file nont found at \"{0}\"", potraitsPath);
 			}
 			else
 				Console.WriteLine("Smite not found");
-
 
 			if (ConfigurationManager.AppSettings.Get("WaitOnDone") == "true")
 			{
@@ -30,6 +45,16 @@ namespace SmiteAnalyzer
 			else
 			{
 				Console.WriteLine("Done");
+			}
+		}
+
+		private void AnalyzePotraits(string path)
+		{
+			var package = UnrealLoader.LoadFullPackage(path);
+
+			foreach (var content in package.Objects)
+			{
+				Console.WriteLine("Name: {0}, Class: {1}, Outer: {2}", content.Name, content.Class.Name, content.Outer == null ? "null" : content.Outer.Name);
 			}
 		}
 	}
